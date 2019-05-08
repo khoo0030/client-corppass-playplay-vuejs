49efb0e638ee34545936941d0587d11415f50ab0
Gerald Khoo <khoo0030@e.ntu.edu.sg>
2019-05-08 15:37:36 +0800

    Make simple CorpPass login button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="login">Login to Corp Pass</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  methods: {
+    async login () {
+      try {
+        const response = await axios.get('http://localhost:7002')
+        console.log(response)
+      } catch (e) {
+        console.error(e)
+      }
+    }
   }
 }
 </script>
